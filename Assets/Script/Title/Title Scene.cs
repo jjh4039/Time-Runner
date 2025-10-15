5a01b9d41532e09e94d8ea8968074d4ce43afb58
정재하 <jjh4039@naver.com>
2025-10-15 11:37:38 +0900

    Title Scene v.0.4.5

---
+++ Assets/Script/Title/Title Scene.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TitleScene : MonoBehaviour
+{
+    public TextMeshProUGUI[] selectTexts;
+    public TextMeshProUGUI selectIndexText;
+    public SpriteRenderer BGAlphaSprite;
+    public int selectIndex;
+
+    public void Start()
+    {
+        selectIndex = 0;
+        BGAlphaSprite.color = new Color(BGAlphaSprite.color.r, BGAlphaSprite.color.g, BGAlphaSprite.color.b, 0.7f);
+        StartCoroutine(BGAlpha());
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectIndex--;
+            if (selectIndex < 0) selectIndex = selectTexts.Length - 1;
+            Select();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectIndex++;
+            if (selectIndex >= selectTexts.Length) selectIndex = 0;
+            Select();
+        }
+    }
+
+    public void Select()
+    {
+        for (int i = 0; i < selectTexts.Length; i++)
+        {
+            if (i == selectIndex) selectTexts[i].color = new Color(0.1f, 0.7f, 0.7f);
+            else selectTexts[i].color = new Color(0.7f, 0.7f, 0.7f);
+        }
+
+        selectIndexText.transform.position = new Vector3(selectIndexText.transform.position.x, selectTexts[selectIndex].transform.position.y, selectIndexText.transform.position.z);
+    }
+
+    IEnumerator BGAlpha()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, 2);
+
+        if (randomIndex == 0) BGAlphaSprite.color = new Color(BGAlphaSprite.color.r, BGAlphaSprite.color.g, BGAlphaSprite.color.b, 0.7f);
+        
+        yield return new WaitForSeconds(0.2f);
+
+        BGAlphaSprite.color = new Color(BGAlphaSprite.color.r, BGAlphaSprite.color.g, BGAlphaSprite.color.b, 0.75f);
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(BGAlpha());
+    }
+}
